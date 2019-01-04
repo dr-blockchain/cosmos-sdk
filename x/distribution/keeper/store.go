d5404bf36ad9c88aff94fe11d0328b24c07b9a6e
Christopher Goes <cwgoes@pluranimity.org>
2019-01-04 03:51:10 +0100

    A few bug fixes; simulation testing; testcases

--- x/distribution/keeper/store.go
+++ x/distribution/keeper/store.go
@@ -1,6 +1,8 @@
 package keeper
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/distribution/types"
 )
@@ -73,6 +75,8 @@ func (k Keeper) GetDelegatorStartingInfo(ctx sdk.Context, val sdk.ValAddress, de
 
 // set the starting period associated with a delegator
 func (k Keeper) setDelegatorStartingInfo(ctx sdk.Context, val sdk.ValAddress, del sdk.AccAddress, period types.DelegatorStartingInfo) {
+	fmt.Printf("Set delegator starting info: val %v, del %v, period %v\n",
+		val, del, period)
 	store := ctx.KVStore(k.storeKey)
 	b := k.cdc.MustMarshalBinaryLengthPrefixed(period)
 	store.Set(GetDelegatorStartingInfoKey(val, del), b)
@@ -88,6 +92,8 @@ func (k Keeper) GetValidatorHistoricalRewards(ctx sdk.Context, val sdk.ValAddres
 
 // set historical rewards for a particular period
 func (k Keeper) setValidatorHistoricalRewards(ctx sdk.Context, val sdk.ValAddress, period uint64, rewards types.ValidatorHistoricalRewards) {
+	fmt.Printf("Set validator historical rewards: val %v, period %v, rewards %v\n",
+		val, period, rewards)
 	store := ctx.KVStore(k.storeKey)
 	b := k.cdc.MustMarshalBinaryLengthPrefixed(rewards)
 	store.Set(GetValidatorHistoricalRewardsKey(val, period), b)
@@ -136,4 +142,40 @@ func (k Keeper) SetOutstandingRewards(ctx sdk.Context, rewards types.Outstanding
 	store := ctx.KVStore(k.storeKey)
 	b := k.cdc.MustMarshalBinaryLengthPrefixed(rewards)
 	store.Set(OutstandingRewardsKey, b)
+}
+
+// get slash fraction for height
+func (k Keeper) GetValidatorSlashFraction(ctx sdk.Context, val sdk.ValAddress, height uint64) (fraction types.ValidatorSlashFraction, found bool) {
+	store := ctx.KVStore(k.storeKey)
+	b := store.Get(GetValidatorSlashFractionKey(val, height))
+	if b == nil {
+		return types.ValidatorSlashFraction{}, false
+	}
+	k.cdc.MustUnmarshalBinaryLengthPrefixed(b, &fraction)
+	return fraction, true
+}
+
+// set slash fraction for height
+func (k Keeper) setValidatorSlashFraction(ctx sdk.Context, val sdk.ValAddress, height uint64, fraction types.ValidatorSlashFraction) {
+	store := ctx.KVStore(k.storeKey)
+	b := k.cdc.MustMarshalBinaryLengthPrefixed(fraction)
+	store.Set(GetValidatorSlashFractionKey(val, height), b)
+}
+
+// iterate over slash fractions between heights, inclusive
+func (k Keeper) IterateValidatorSlashFractions(ctx sdk.Context, val sdk.ValAddress, startingHeight uint64, endingHeight uint64,
+	handler func(height uint64, fraction types.ValidatorSlashFraction) (stop bool)) {
+	store := ctx.KVStore(k.storeKey)
+	iter := store.Iterator(
+		GetValidatorSlashFractionKey(val, startingHeight),
+		GetValidatorSlashFractionKey(val, endingHeight+1),
+	)
+	for ; iter.Valid(); iter.Next() {
+		var fraction types.ValidatorSlashFraction
+		k.cdc.MustUnmarshalBinaryLengthPrefixed(iter.Value(), &fraction)
+		height := GetValidatorSlashFractionHeight(iter.Key())
+		if handler(height, fraction) {
+			break
+		}
+	}
 }
